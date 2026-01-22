4b565a33376c93a367934664e7d8916f32b7b6b6
Reinald Santos <webmaster@epf.local>
2026-01-22 10:29:37 +0000

    🚨 URGENTE: Fix SSL self-signed certificate for Render

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-﻿// config/database.js - ACEITA SSL auto-assinado
+﻿// config/database.js - CONFIGURAÇÃO DEFINITIVA PARA RENDER
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
       ssl: {
-        rejectUnauthorized: false, // ⚠️ ACEITA certificado auto-assinado
+        rejectUnauthorized: false, // ⚠️ ACEITA certificado auto-assinado do Render
       },
     },
     pool: { min: 0, max: 5 },
