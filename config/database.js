dbbf8b003c932fd309cc2eaeff83a9adba1a6f81
Reinald Santos <webmaster@epf.local>
2026-01-22 10:24:23 +0000

    🔧 FIX: Aceita SSL auto-assinado do PostgreSQL Render

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
-﻿// config/database.js
+﻿// config/database.js - ACEITA SSL auto-assinado
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
+      ssl: {
+        rejectUnauthorized: false, // ⚠️ ACEITA certificado auto-assinado
+      },
     },
     pool: { min: 0, max: 5 },
-    acquireConnectionTimeout: 600000,
   },
 });
