4bcdf79fb12298499a7a56dff2050d59fe3b1347
Reinald Santos <webmaster@epf.local>
2026-01-23 10:53:40 +0000

    FIX: Corrigido config/database.js e dependências

--- config/database.js
+++ config/database.js
@@ -1,10 +1,32 @@
-﻿// config/database.js - VERSÃO SIMPLES PARA NOVA URL
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+﻿const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  if (env('DATABASE_URL')) {
+    const config = parse(env('DATABASE_URL'));
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: env.bool('DATABASE_SSL', false) ? {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          } : false,
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      connectionString: env('DATABASE_URL'),
+      client: 'sqlite',
+      connection: {
+        filename: '.tmp/data.db',
+      },
+      useNullAsDefault: true,
     },
-    pool: { min: 0, max: 5 },
-  },
-});
+  };
+};
