c2336a0f444c56f1b9da3bf026d4899fc3f6011e
Reinald Santos <webmaster@epf.local>
2026-01-26 15:30:38 +0000

    Fix: Convert config files to .js, fix tsconfig.json, successful build

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,46 +1,7 @@
-﻿// config/middlewares.js
-module.exports = [
+﻿module.exports = [
   'strapi::errors',
-  {
-    name: 'global::emergency-public',
   'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'strapi-escola-final.onrender.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'strapi-escola-final.onrender.com',
-          ],
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'https://site-escola-five-sand.vercel.app',
-        'https://strapi-escola-final.onrender.com',
-      ],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -49,4 +10,3 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
-
