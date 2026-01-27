897eb3cdb951e04dca9a82c681271ad6b122c20c
Reinald Santos <webmaster@epf.local>
2026-01-27 10:49:46 +0000

    Corrigido: Configuração de porta para Render (PORT=3000)

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+module.exports = ( env ) => (  echo   host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 3000),
   app: {
     keys: env.array('APP_KEYS'),
   },
-});
+);
